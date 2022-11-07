00adbdef1a32029df22fb2a381cae582da722b5a
FischLord <46058672+FischLord@users.noreply.github.com>
2022-11-07 10:17:50 +0100

    import für colorama (Farbefekte) hinzugefügt

--- programming/python/connect4/connect4.py
+++ programming/python/connect4/connect4.py
@@ -1,6 +1,8 @@
 # python game "4 winns" in shell
 # 07.11.2022 Leon Kohlhaußen
 # contributors Janneck Lehmann
+from colorama import *
+init(autoreset=True) #there is no need of reset every color effekt in the code duo this setting
 
 # define the board
 def board():
@@ -17,7 +19,7 @@ def board():
     print("+---+---+---+---+---+---+---+---+")
     print("| 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |")
     print("+---+---+---+---+---+---+---+---+")
-    print("| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |")
+    print(Back.GREEN + "| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |")
 
 
 
@@ -27,3 +29,4 @@ print("the rules:")
 print("1. You can only place a stone in a column that is not full.")
 print("2. You can only put a stone in the lowest row of a column. ")
 print("3. You can only win if you have 4 stones in a row, column or diagonal.")
+board()

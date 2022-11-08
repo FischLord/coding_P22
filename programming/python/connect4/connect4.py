fda4d9314c447cf55056b19d5a99ca2858fab535
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2022-11-08 15:55:19 +0100

    New symbol

--- programming/python/connect4/connect4.py
+++ programming/python/connect4/connect4.py
@@ -1,17 +1,22 @@
-# python game "4 winns" in shell
+# python game "4 wins/connect4/4 Gewinnt" in shell
 # 07.11.2022 Leon Kohlhaußen
 # contributors Janneck Lehmann
 
-#<<<<<<<<<<< imports >>>>>>>>>>>>>>>>>
-from colorama import * #https://hellocoding.de/blog/coding-language/python/farben-im-terminal
-init(autoreset=True) #there is no need of reset every color effekt in the code duo this setting
+# <<<<<<<<<<< imports >>>>>>>>>>>>>>>>>
+# https://hellocoding.de/blog/coding-language/python/farben-im-terminal
+import random  # random module to start with a random player
+from colorama import *
+# there is no need of reset every color effekt in the code duo this setting
+init(autoreset=True)
 
 
-#<<<<<<<<<< variables >>>>>>>>>>>>>>>>>
+# <<<<<<<<<< variables >>>>>>>>>>>>>>>>>
 gamenotover = True
 player = "player1"
 
 # welcome message when game is started
+
+
 def startMessage():
     print("Welcome to 4wins!")
     print("the rules:")
@@ -20,33 +25,52 @@ def startMessage():
     print("3. You can only win if you have 4 stones in a row, column or diagonal.")
 
 
-#define the board as a list in a 8x8 matrix
+# define the board as a list in a 8x8 matrix
 def board():
-    lf = "  " #lf = leeres feld
+    lf = " 🔴 "  # lf = leeres feld
     grid = []
     for x in range(64):
         grid.append(lf)
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[0],"|",grid[1],"|",grid[2],"|",grid[3],"|",grid[4],"|",grid[5],"|",grid[6],"|",grid[7],"|")
+    print("|", grid[0], "|", grid[1], "|", grid[2], "|", grid[3],
+          "|", grid[4], "|", grid[5], "|", grid[6], "|", grid[7], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[8],"|",grid[9],"|",grid[10],"|",grid[11],"|",grid[12],"|",grid[13],"|",grid[14],"|",grid[15],"|")
+    print("|", grid[8], "|", grid[9], "|", grid[10], "|", grid[11],
+          "|", grid[12], "|", grid[13], "|", grid[14], "|", grid[15], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[16],"|",grid[17],"|",grid[18],"|",grid[19],"|",grid[20],"|",grid[21],"|",grid[22],"|",grid[23],"|")
+    print("|", grid[16], "|", grid[17], "|", grid[18], "|", grid[19],
+          "|", grid[20], "|", grid[21], "|", grid[22], "|", grid[23], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[24],"|",grid[25],"|",grid[26],"|",grid[27],"|",grid[28],"|",grid[29],"|",grid[30],"|",grid[31],"|")
+    print("|", grid[24], "|", grid[25], "|", grid[26], "|", grid[27],
+          "|", grid[28], "|", grid[29], "|", grid[30], "|", grid[31], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[32],"|",grid[33],"|",grid[34],"|",grid[35],"|",grid[36],"|",grid[37],"|",grid[38],"|",grid[39],"|")
+    print("|", grid[32], "|", grid[33], "|", grid[34], "|", grid[35],
+          "|", grid[36], "|", grid[37], "|", grid[38], "|", grid[39], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[40],"|",grid[41],"|",grid[42],"|",grid[43],"|",grid[44],"|",grid[45],"|",grid[46],"|",grid[47],"|")
+    print("|", grid[40], "|", grid[41], "|", grid[42], "|", grid[43],
+          "|", grid[44], "|", grid[45], "|", grid[46], "|", grid[47], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[48],"|",grid[49],"|",grid[50],"|",grid[51],"|",grid[52],"|",grid[53],"|",grid[54],"|",grid[55],"|")
+    print("|", grid[48], "|", grid[49], "|", grid[50], "|", grid[51],
+          "|", grid[52], "|", grid[53], "|", grid[54], "|", grid[55], "|")
     print("+----+----+----+----+----+----+----+----+")
-    print("|",grid[56],"|",grid[57],"|",grid[58],"|",grid[59],"|",grid[60],"|",grid[61],"|",grid[62],"|",grid[63],"|")
+    print("|", grid[56], "|", grid[57], "|", grid[58], "|", grid[59],
+          "|", grid[60], "|", grid[61], "|", grid[62], "|", grid[63], "|")
     print("+----+----+----+----+----+----+----+----+")
     print(Back.GREEN + "| 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  |")
 
 
-#switching player after each move 
+# internal grid for the game to run in the background
+
+# start the game with a random player
+def randomplayer():
+    global player
+    player = random.choice(["player1", "player2"])
+    print(Fore.YELLOW + player + " starts the game")
+    move()
+
+# switching player after each move
+
+
 def switchPlayer():
     global player
     if player == "player1":
@@ -54,7 +78,9 @@ def switchPlayer():
     else:
         player = "player1"
 
-#player input for column question and validation
+# player input for column question and validation
+
+
 def playerinput():
     while True:
         print(Back.WHITE + player + " choose a column")
@@ -66,26 +92,23 @@ def playerinput():
             else:
                 break
         except ValueError:
-            print(Fore.RED + "please enter a number")  
+            print(Fore.RED + "please enter a number")
 
-#player choose a column
+# player choose a column
+
+
 def move():
     while gamenotover:
         playerinput()
         switchPlayer()
-        
+
+# check if column is full and if not place a stone in the lowest row
 
 
-#grid will be filled with the players color when this is an right move
+# grid will be filled with the players color when this is an right move
 
 
-
-
-
-
-
-
-#<<<<<<<<<<<<<<< wincheck >>>>>>>>>>>>>>>>>>
+# <<<<<<<<<<<<<<< wincheck >>>>>>>>>>>>>>>>>>
 
 # check if the game is won
 # check if a row is full
@@ -93,8 +116,8 @@ def move():
 # check if a diagonal is full
 # check if the game is a draw
 
-
-
+board()
 startMessage()
+randomplayer()
 board()
 move()
